cf294c8c6d3bb15087ae9a5ab5f0aeee58bcea37
Seth Ladd <sethladd@google.com>
2014-01-10 13:51:23 -0800

    some style tweaks

--- web/Camera.dart
+++ web/Camera.dart
@@ -2,101 +2,99 @@ import 'dart:html';
 import 'dart:async';
 
 class Camera {
-  
+
   static final int COUNTDOWN_TIME = 3000;
-  
-  MediaStream _cameraStream;
-  CanvasElement _cameraView;
-  CanvasRenderingContext2D _cameraViewContext;
-  VideoElement _video;
-  
-  int _width;
-  int _height;
-  bool _isTakingPhoto;
-  double _photoStartTime;
-  Completer _photoCompleter;
-  double _flashValue;
-  
-  Camera(CanvasElement cameraView) {
-    
-    _isTakingPhoto = false;
-    _flashValue = 0.0;
-   
-    _video = new VideoElement();
-    _video.autoplay = true;
-    
-    _cameraView = cameraView;
-    
-    _width = _cameraView.width;
-    _height = _cameraView.height;
-    
-    _cameraViewContext = _cameraView.getContext('2d');
-    _cameraViewContext.fillRect(0, 0, _width, _height);
+
+  MediaStream cameraStream;
+  CanvasElement cameraView;
+  CanvasRenderingContext2D cameraViewContext;
+  VideoElement video;
+
+  int width;
+  int height;
+  bool isTakingPhoto = false;
+  double photoStartTime;
+  Completer photoCompleter;
+  double flashValue = 0.0;
+
+  Camera(this.cameraView) {
+
+    video = new VideoElement()
+      ..autoplay = true;
+
+    width = cameraView.width;
+    height = cameraView.height;
+
+    cameraViewContext = cameraView.getContext('2d') as CanvasRenderingContext2D
+        ..fillRect(0, 0, width, height);
   }
-  
-  void start() {
-    
-    if (_cameraStream != null)
-      return;
-    
-    window.navigator.getUserMedia(video: true)
-      .then(_onUserMediaStarted)
-      .catchError(_onUserMediaError);
+
+  Future start() {
+
+    if (cameraStream != null) {
+      throw new StateError('camera already started');
+    }
+
+    return window.navigator.getUserMedia(video: true)
+      .then(_onUserMediaStarted);
   }
-  
+
   Future<String> takePhoto() {
-    
-    _isTakingPhoto = true;
-    _photoStartTime = window.performance.now();
-    _photoCompleter = new Completer();
-    _flashValue = 0.0;
-   
-    return _photoCompleter.future;
+
+    isTakingPhoto = true;
+    photoStartTime = window.performance.now();
+    photoCompleter = new Completer();
+    flashValue = 0.0;
+
+    return photoCompleter.future;
   }
-  
+
   void _updateCameraView(double time) {
-    _cameraViewContext.drawImage(_video, 0, 0);
-    
-    if (_isTakingPhoto) {
-      
-      double timeSincePhotoRequest = (time - _photoStartTime);
-      
+    cameraViewContext.drawImage(video, 0, 0);
+
+    if (isTakingPhoto) {
+
+      double timeSincePhotoRequest = (time - photoStartTime);
+
       if (timeSincePhotoRequest > Camera.COUNTDOWN_TIME) {
-        
-        _isTakingPhoto = false;
-        _flashValue = 1.0;
-        _photoCompleter.complete(_cameraView.toDataUrl());
-        
+
+        isTakingPhoto = false;
+        flashValue = 1.0;
+        photoCompleter.complete(cameraView.toDataUrl());
+
       } else {
-        
-        String timeSincePhotoRequestAsString = ((Camera.COUNTDOWN_TIME - timeSincePhotoRequest) * 0.001).ceil().toString();
-        _cameraViewContext.font = 'bold 180px Arial';
-        _cameraViewContext.textAlign = 'center';
-        _cameraViewContext.textBaseline = 'middle';
-        _cameraViewContext.fillStyle = '#FFF';
-        _cameraViewContext.shadowOffsetY = 3;
-        _cameraViewContext.shadowBlur = 3;
-        _cameraViewContext.shadowColor = 'rgba(0,0,0,0.4)';
-        _cameraViewContext.fillText(timeSincePhotoRequestAsString, _width / 2, _height / 2);
+
+        String timeSincePhotoRequestAsString = _timeSincePhotoRequest(timeSincePhotoRequest);
+
+        cameraViewContext
+          ..font = 'bold 180px Arial'
+          ..textAlign = 'center'
+          ..textBaseline = 'middle'
+          ..fillStyle = '#FFF'
+          ..shadowOffsetY = 3
+          ..shadowBlur = 3
+          ..shadowColor = 'rgba(0,0,0,0.4)'
+          ..fillText(timeSincePhotoRequestAsString, width / 2, height / 2);
       }
     }
-    
-    _cameraViewContext.fillStyle = 'rgba(255, 255, 255, $_flashValue)';
-    _cameraViewContext.fillRect(0, 0, _width, _height);
-    
-    _flashValue += (0.0 - _flashValue) * 0.03;
+
+    cameraViewContext
+      ..fillStyle = 'rgba(255, 255, 255, $flashValue)'
+      ..fillRect(0, 0, width, height);
+
+    flashValue += (0.0 - flashValue) * 0.03;
     window.animationFrame.then(_updateCameraView);
   }
-  
+
   void _onUserMediaStarted(MediaStream cameraStream) {
-    _cameraStream = cameraStream;
-    _video.src = Url.createObjectUrlFromStream(cameraStream);
+    cameraStream = cameraStream;
+    video.src = Url.createObjectUrlFromStream(cameraStream);
     window.animationFrame.then(_updateCameraView);
   }
-  
-  void _onUserMediaError(e) {
-    print("Lolz. Error: $e");
+
+  String _timeSincePhotoRequest(timeSincePhotoRequest) {
+    return ((Camera.COUNTDOWN_TIME - timeSincePhotoRequest) * 0.001).ceil().toString();
   }
-  
-  bool get canTakePhoto => !_isTakingPhoto;
+
+  bool get canTakePhoto => !isTakingPhoto;
 }
--- web/snapbooth.dart
+++ web/snapbooth.dart
@@ -1,9 +1,9 @@
 import 'dart:html';
-import 'Camera.dart';
+import 'camera.dart';
 
 Camera camera;
 DivElement photoContainer;
-int photoCount;
+int photoCount = 0;
 
 final int CONTAINER_PADDING = 5;
 final int SNAPSHOT_WIDTH = 140;
@@ -11,37 +11,36 @@ final int SNAPSHOT_PADDING = 14;
 final int SNAPSHOT_TOTAL_WIDTH = SNAPSHOT_WIDTH + SNAPSHOT_PADDING;
 
 void main() {
- 
-  CanvasElement cameraView = querySelector('.video-container canvas');
-  ButtonElement takePhotoButton = document.getElementById('take-photo');
+
+  CanvasElement cameraView = querySelector('.video-container canvas') as CanvasElement
+      ..width = 640
+      ..height = 480;
+  ButtonElement takePhotoButton = querySelector('#take-photo');
   photoContainer = querySelector('.taken-photos-inner');
-  
-  cameraView.width = 640;
-  cameraView.height = 480;
-  
-  photoCount = 0;
-  
-  camera = new Camera(cameraView);
-  camera.start();
-  
+
+  camera = new Camera(cameraView)
+    ..start();
+
   takePhotoButton.onClick.listen(takePhoto);
 }
 
 void takePhoto(_) {
-  if (camera.canTakePhoto)
+  if (camera.canTakePhoto) {
     camera.takePhoto().then(onPhotoTaken);
+  }
 }
 
 void onPhotoTaken(String photoData) {
-  ImageElement snapshot = new ImageElement();
-  snapshot.src = photoData;
-  snapshot.width = SNAPSHOT_WIDTH;
-  
+  ImageElement snapshot = new ImageElement()
+    ..src = photoData
+    ..width = SNAPSHOT_WIDTH;
+
   photoCount++;
-  
+
   int newWidth = (CONTAINER_PADDING + photoCount * SNAPSHOT_TOTAL_WIDTH);
 
-  photoContainer.style.width = newWidth.toString() + 'px';
-  photoContainer.children.add(snapshot);
-  photoContainer.parent.scrollLeft = newWidth;
+  photoContainer
+    ..style.width = '${newWidth}px'
+    ..children.add(snapshot)
+    ..parent.scrollLeft = newWidth;
 }
